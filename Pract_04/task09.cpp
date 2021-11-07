6ac5321e01eb03789858dba1952f8ce22482de50
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-11-07 17:59:21 +0200

    Update task09.cpp

--- Pract_04/task09.cpp
+++ Pract_04/task09.cpp
@@ -7,11 +7,11 @@ int main()
       	cin >> a >> b >> c >> d >> e;
 
        	double fx;
-	      for (int x = -100; x <= 100; x++)
+	for (int x = -100; x <= 100; x++)
       	{
-	              fx = a * x*x*x*x + b * x*x*x + c * x*x + d * x + e;
-		            if (fx >= 0)
-			                  cout << x << ' ';
+	        fx = a * x*x*x*x + b * x*x*x + c * x*x + d * x + e;
+	        if (fx >= 0)
+		        cout << x << ' ';
       	}
   
       	cout << endl;
