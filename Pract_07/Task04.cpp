479b623c8b8bf16e1b36417ec548a6da8abe7806
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-11-18 23:56:45 +0200

    Update Task04.cpp

--- Pract_07/Task04.cpp
+++ Pract_07/Task04.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+using namespace std;
 
-void printArray(int arr[], int size)
+void printArray(const int arr[], int size)
 {
 	for (size_t i = 0; i < size; i++)
-		std::cout << arr[i] << ' ';
-	std::cout << '\n';
+		cout << arr[i] << ' ';
+	cout << endl;
 }
 
-void convertInTriton(int arr[], int size)
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+ 	b = temp;
+}
+
+void convertToTrion(int arr[], int size)
 {
 	for (int i = 1; i < size - 1; i++)
 	{
-		if ((arr[i - 1] < arr[i] && (arr[i] < arr[i + 1]) || (arr[i - 1] > arr[i] && arr[i] > arr[i + 1])))
-			std::swap(arr[i], arr[i + 1]);
+		if ((arr[i - 1] < arr[i] && arr[i] < arr[i + 1]) || (arr[i - 1] > arr[i] && arr[i] > arr[i + 1]))
+			swap(arr[i], arr[i + 1]);
 	}
 
 	printArray(arr, size);
