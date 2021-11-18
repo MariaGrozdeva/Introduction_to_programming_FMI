49827841a11e7f81898dcdf7cabe119311ba0cec
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-11-19 00:14:47 +0200

    Update Task08.cpp

--- Pract_07/Task08.cpp
+++ Pract_07/Task08.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
+using namespace std;
 
-//If the array contain given number
-bool containNumber(int arr[], int size, int num)
+// If the array contains given number
+bool containsNumber(const int arr[], int size, int num)
 {
 	for (int i = 0; i < size; i++)
 	{
 		if (arr[i] == num)
 			return true;
 	}
-
 	return false;
 }
 
-bool isTheSameSet(int arr1[], int size1, int arr2[], int size2)
+bool areSequencesOfSameSet(int arr1[], int size1, int arr2[], int size2)
 {
-	if (size1 >= size2)
+	for (int i = 0; i < size1; i++)
 	{
-		for (int i = 0; i < size1; i++)
-		{
-			if (!containNumber(arr2, size2, arr1[i]))
-				return false;
-		}
-	}
-	else
-	{
-		for (int i = 0; i < size2; i++)
-		{
-			if (!containNumber(arr1, size1, arr2[i]))
-				return false;
-		}
+		if (!containsNumber(arr2, size2, arr1[i]))
+			return false;
 	}
 
+	for (int i = 0; i < size2; i++)
+	{
+		if (!containsNumber(arr1, size1, arr2[i]))
+			return false;
+	}
+	
 	return true;
 }
 
@@ -42,7 +37,7 @@ int main()
 	int arr2[] = { 1, 3, 3, 4, 1, 0, 3 };
 	int len2 = sizeof(arr2) / sizeof(int);
 
-	std::cout << (isTheSameSet(arr1, len1, arr2, len2) ? "Yes" : "No");
+	cout << areSequencesOfSameSet(arr1, len1, arr2, len2) ? "Yes" : "No";
 
 	return 0;
 }
