1f4632e7ca0579d7c4c11dc7342c4073221443ad
Nick-Raychev <92603768+Nick-Raychev@users.noreply.github.com>
2021-11-23 21:59:35 +0200

    Add files via upload

---
+++ Pract_08/task05.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+using namespace std;
+
+void returnBigLetters(const char arr[])
+{
+    int len = strlen(arr);
+    for (int i = 0; i < len; i++)
+    {  
+        if (arr[i] >= 'a' && arr[i] <= 'z')
+        {
+            
+            char c = arr[i];
+            c -= 32;
+            cout << c;
+        }
+        else
+            cout << arr[i];
+    }
+}
+
+int main()
+{
+    const int maxSize = 64;
+    char arr[maxSize];
+
+    cin.getline(arr, maxSize);
+
+    returnBigLetters(arr);
+}
