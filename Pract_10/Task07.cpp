db89a0b284c121f7099d4e7907628294e39dc8d5
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-12-15 14:51:15 +0200

    Update Task07.cpp

--- Pract_10/Task07.cpp
+++ Pract_10/Task07.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
 
 const int SIZE = 4;
+
+void printMatrix(const int mat[SIZE][SIZE])
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+			cout << mat[i][j] << ' ';
+		cout << endl;
+	}
+	cout << endl;
+}
 
 void rotateMatrixClockWise(int mat[SIZE][SIZE])
 {
@@ -9,6 +21,7 @@ void rotateMatrixClockWise(int mat[SIZE][SIZE])
 		for (int j = i; j < SIZE - i - 1; j++) 
 		{
 			int temp = mat[i][j];
+			
 			*(*(mat + i) + j) = *(*(mat + (SIZE - j - 1)) + i);
 			*(*(mat + (SIZE - j - 1)) + i) = *(*(mat + (SIZE - i - 1)) + (SIZE - j - 1));
 			*(*(mat + (SIZE - i - 1)) + (SIZE - j - 1)) = *(*(mat + j) + (SIZE - i - 1));
@@ -17,26 +30,11 @@ void rotateMatrixClockWise(int mat[SIZE][SIZE])
 	}
 }
 
-
-void printMatrix(const int mat[SIZE][SIZE])
-{
-	for (int i = 0; i < SIZE; i++)
-	{
-		for (int j = 0; j < SIZE; j++)
-		{
-			std::cout << mat[i][j] << ' ';
-		}
-		std::cout << '\n';
-	}
-	std::cout << '\n';
-}
-
 int main()
 {
 	int mat[SIZE][SIZE] = { {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16} };
 	printMatrix(mat);
+	
 	rotateMatrixClockWise(mat);
 	printMatrix(mat);
-
-	return 0;
 }
