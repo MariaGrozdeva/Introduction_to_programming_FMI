1c1316d8d656deea7dc312236f4b55340ff9742d
MariaGrozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2021-12-20 19:35:22 +0200

    Update Task06.cpp

--- Pract_11/Task06.cpp
+++ Pract_11/Task06.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+using namespace std;
 
 void readArray(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
-		std::cin >> arr[i];
-	std::cout << '\n';
+		cin >> arr[i];
+	cout << endl;
 }
-
-void printArray(int* arr, int size)
+void printArray(const int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
-		std::cout << arr[i] << ' ';
-	std::cout << '\n';
+		cout << arr[i] << ' ';
+	cout << endl;
 }
 
 void merge(int* arr1, int size1, int* arr2, int size2, int*& result) // Only for sorted arrays
@@ -28,50 +28,47 @@ void merge(int* arr1, int size1, int* arr2, int size2, int*& result) // Only for
 			result[resIter++] = arr2[arr2Iter++];
 	}
 
-	if (arr1Iter == size1)
-	{
-		for (int i = arr2Iter; i < size2; i++)
-			result[resIter++] = arr2[i];
-	}
-	else if (arr2Iter == size2)
-	{
-		for (int i = arr1Iter; i < size1; i++)
-			result[resIter++] = arr1[i];
-	}
-	
+	for (int i = arr2Iter; i < size2; i++)
+		result[resIter++] = arr2[i];
+	for (int i = arr1Iter; i < size1; i++)
+		result[resIter++] = arr1[i];
 }
 
 int main()
 {
 	int n1;
-	std::cout << "n1 = ";
-	std::cin >> n1;
+	cout << "n1 = ";
+	cin >> n1;
+	
 	int* arr1 = new int[n1];
 	if (!arr1)
 	{
-		std::cerr << "Can't allocate memory!\n";
+		cerr << "Can't allocate memory!\n";
 		return -1;
 	}
-	std::cout << "Input sorted array: ";
+	
+	cout << "Input sorted array: ";
 	readArray(arr1, n1); 
 
 	int n2;
-	std::cout << "n2 = ";
-	std::cin >> n2;
+	cout << "n2 = ";
+	cin >> n2;
+	
 	int* arr2 = new int[n2];
 	if (!arr2)
 	{
-		std::cerr << "Can't allocate memory!\n";
+		cerr << "Can't allocate memory!\n";
 		return -1;
 	}
-	std::cout << "Input sorted array: ";
+	
+	cout << "Input sorted array: ";
 	readArray(arr2, n2);
 
 	int newSize = n1 + n2;
 	int* newArr = new int[newSize];
 	if (!newArr)
 	{
-		std::cerr << "Can't allocate memory!\n";
+		cerr << "Can't allocate memory!\n";
 		return -1;
 	}
 
@@ -81,6 +78,4 @@ int main()
 	delete[] arr1;
 	delete[] arr2;
 	delete[] newArr;
-
-	return 0;
 }
