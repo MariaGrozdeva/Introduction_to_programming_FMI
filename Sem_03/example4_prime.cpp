f99277b448f301d1b1dd1c4bb894950ec97d2770
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2022-10-25 19:36:01 +0300

    Add files via upload

---
+++ Sem_03/example4_prime.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned n;
+	cin >> n;
+
+	bool isPrime = true;
+	double sqrtOfN = sqrt(n);
+
+	if (n <= 1)
+	{
+		isPrime = false;
+	}
+	for (size_t divisorCandidate = 2; divisorCandidate <= sqrtOfN; divisorCandidate++)
+	{
+		if (n % divisorCandidate == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+	cout << isPrime << endl;
+}
