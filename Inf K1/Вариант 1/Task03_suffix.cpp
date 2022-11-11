5bbc808b0d90a6b4a5ffacec31802f3ce7ced0e0
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2022-11-11 16:53:34 +0200

    Add files via upload

--- Inf K1/Вариант 1/Task03_suffix.cpp
+++ Inf K1/Вариант 1/Task03_suffix.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+unsigned reverse(unsigned n)
+{
+	unsigned reversed = 0;
+	while (n != 0)
+	{
+		short lastDigit = n % 10;
+		(reversed *= 10) += lastDigit;
+		n /= 10;
+	}
+
+	return reversed;
+}
+bool isPalindrome(unsigned n)
+{
+	return n == reverse(n);
+}
+
 int main()
 {
 	unsigned n;
@@ -9,17 +26,7 @@ int main()
 	for (size_t i = 10, currentSuffix = 0; n != currentSuffix; i *= 10)
 	{
 		currentSuffix = n % i;
-
-		unsigned tempCurrentSuffix = currentSuffix;
-		unsigned reversedCurrentSuffix = 0;
-		while (tempCurrentSuffix != 0)
-		{
-			short lastDigit = tempCurrentSuffix % 10;
-			(reversedCurrentSuffix *= 10) += lastDigit;
-			tempCurrentSuffix /= 10;
-		}
-
-		if (currentSuffix == reversedCurrentSuffix)
+		if (isPalindrome(currentSuffix))
 		{
 			cout << currentSuffix << endl;
 		}
