256eaeb20f2030a967987292f5d787faf4f27e13
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-11-21 00:10:21 +0200

    Update Task02.cpp

--- Sem_03/Solutions/Task02.cpp
+++ Sem_03/Solutions/Task02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -7,21 +8,27 @@ int main()
 	cin >> a >> b;
 
 	if (a < 2)
+	{
 		a = 2;
+	}
 
 	int number = 0;
-	for (int i = a; i <= b; i++)
+	for (size_t i = a; i <= b; i++)
 	{
 		bool prime = true;
-		double temp = sqrt(i);
+		double sqrtI = sqrt(i);
 		
-		for (int j = 2; j <= temp; j++) // N.B.!! checks for divisors ONLY IN THE RANGE [2..sqrt(i)]. If there aren't any divisors in this interval, then there are NO DIVISORS AT ALL!
+		for (int j = 2; j <= sqrtI; j++) // N.B.!! checks for divisors ONLY IN THE RANGE [2..sqrt(i)]. If there aren't any divisors in this interval, then there are NO DIVISORS AT ALL!
 		{
 			if (!(i % j))
+			{
 				prime = false;
+			}
 		}
 		
 		if (prime)
+		{
 			cout << i << ' ';
+		}
 	}
 }
