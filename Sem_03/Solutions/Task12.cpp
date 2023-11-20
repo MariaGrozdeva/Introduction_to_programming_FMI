afdccda3a94f05f48cb63724cc4c71e0904006f3
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2023-11-21 00:05:08 +0200

    Update Task12.cpp

--- Sem_03/Solutions/Task12.cpp
+++ Sem_03/Solutions/Task12.cpp
@@ -9,39 +9,55 @@ int main()
 	// Upper part of the hourglass
 	int numOfStars = 2 * n + 1;
 
-	for (int i = 0; i < n; i++)
+	for (size_t i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (size_t j = 0; j < i; j++)
+		{
 			cout << ' ';
-		for (int j = 0; j < numOfStars; j++)
+		}
+		for (size_t j = 0; j < numOfStars; j++)
+		{
 			cout << '*';
-		for (int j = 0; j < i; j++)
+		}
+		for (size_t j = 0; j < i; j++)
+		{
 			cout << ' ';
+		}
 
 		numOfStars -= 2;
 		cout << endl;
 	} // numOfStars = 1;
 
 	for (int i = 0; i < n; i++)
+	{
 		cout << ' ';
+	}
 
 	cout << '*'; // middle star
 
 	for (int i = 0; i < n; i++)
+	{
 		cout << ' ';
+	}
 	cout << endl;
 
 	// Lower part of the hourglass
-	for (int i = 0; i < n; i++)
+	for (size_t i = 0; i < n; i++)
 	{
 		numOfStars += 2;
 
-		for (int j = 0; j < n - i - 1; j++)
+		for (size_t j = 0; j < n - i - 1; j++)
+		{
 			cout << ' ';
-		for (int j = 0; j < numOfStars; j++)
+		}
+		for (size_t j = 0; j < numOfStars; j++)
+		{
 			cout << '*';
-		for (int j = 0; j < n - i - 1; j++)
+		}
+		for (size_t j = 0; j < n - i - 1; j++)
+		{
 			cout << ' ';
+		}
 
 		cout << endl;
 	}
