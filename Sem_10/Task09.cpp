c057e1e5ff77c527851f1b63371867b42da87fd6
Maria Grozdeva <64608453+MariaGrozdeva@users.noreply.github.com>
2024-01-20 17:42:00 +0200

    Update Task09.cpp

--- Sem_10/Task09.cpp
+++ Sem_10/Task09.cpp
@@ -9,9 +9,20 @@ bool isDigit(char ch)
 	return ch >= '0' && ch <= '9';
 }
 
+short getDigitsCount(unsigned int num)
+{
+    short count = 0;
+    while (num)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
 bool containsNinesOnly(const char*& str)
 {
-	while (*str != '\0' && isDigit(*str))
+	while (*str && isDigit(*str))
 	{
 		if (*str != '9')
 		{
@@ -25,7 +36,7 @@ bool containsNinesOnly(const char*& str)
 unsigned int getNumsWithNinesOnlyCount(const char* str)
 {
 	unsigned int count = 0;
-	while (*str != '\0')
+	while (*str)
 	{
 		if (*str == '9' && containsNinesOnly(++str))
 		{
@@ -36,10 +47,10 @@ unsigned int getNumsWithNinesOnlyCount(const char* str)
 	return count;
 }
 
-unsigned int getNumber(const char*& str)
+unsigned int stringToNumber(const char*& str)
 {
 	unsigned int number = 0;
-	while (*str != '\0' && isDigit(*str))
+	while (*str && isDigit(*str))
 	{
 		(number *= 10) += *str - '0';
 		str++;
@@ -47,25 +58,39 @@ unsigned int getNumber(const char*& str)
 	return number;
 }
 
-char* incrementEachNumber(const char* str)
+const char* numberToString(unsigned int number)
+{
+    short digitsCount = getDigitsCount(number);
+    char* res = new char[digitsCount + 1];
+    for (int i = digitsCount - 1; i >= 0; i--)
+    {
+        res[i] = number % 10 + '0';
+        number /= 10;
+    }
+    res[digitsCount] = '\0';
+    return res;
+}
+
+const char* incrementEachNumber(const char* str)
 {
 	unsigned int numsWithNinesOnlyCount = getNumsWithNinesOnlyCount(str);
 	char* resultStr = new char[strlen(str) + numsWithNinesOnlyCount + 1];
 	*resultStr = '\0';
 
-	while (*str != '\0')
+	while (*str)
 	{
 		if (!isDigit(*str))
 		{
 			char toAppend[2] = { *str, '\0' };
 			strcat(resultStr, toAppend);
-
 			str++;
 		}
 		else
 		{
-			unsigned int incrementedNum = getNumber(str) + 1;
-			strcat(resultStr, std::to_string(incrementedNum).c_str());
+			unsigned int incrementedNumber = stringToNumber(str) + 1;
+			const char* incrementedNumberAsString = numberToString(incrementedNumber);
+			strcat(resultStr, incrementedNumberAsString);
+			delete[] incrementedNumberAsString;
 		}
 	}
 
@@ -74,11 +99,11 @@ char* incrementEachNumber(const char* str)
 
 int main()
 {
-	const size_t MAX_SIZE = 1024;
-	char str[MAX_SIZE];
+	constexpr size_t MAX_SIZE = 1024;
+	char str[MAX_SIZE + 1];
 	std::cin.getline(str, MAX_SIZE);
 
-	char* result = incrementEachNumber(str);
+	const char* result = incrementEachNumber(str);
 	std::cout << result;
 
 	delete[] result;
