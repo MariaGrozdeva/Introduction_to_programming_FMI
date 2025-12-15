1dcd30da4aa6bf4eae0217bd950ec8781da8bcdf
MartinTs168 <martin.tsenev@gmail.com>
2025-12-15 17:26:32 +0200

    fixed task 5

--- C/Sem_07/task_5.c
+++ C/Sem_07/task_5.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <limits.h>
 
+unsigned short getBitCount(int value)
+{
+    unsigned short count = 0;
+    while (value != 0)
+    {
+        count++;
+        value >>= 1;
+    }
+    return count;
+}
+
 int isNumberContained(int n, int k)
 {
     unsigned short size = sizeof(int) * CHAR_BIT;
-
+    int mask = (1 << getBitCount(k)) - 1;
     for (int i = 0; i < size; i++)
     {
-        if (k == ((n >> i) & k))
+        if (((n >> i) & mask) == k)
         {
             return 1;
         }
